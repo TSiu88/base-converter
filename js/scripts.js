d47f119835b191073385aa8c19a9058f66ad1694
jason mcgrady and Tiffany Siu <jason@>
2020-01-23 16:27:59 -0800

    add conversion function

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+function convert(base, eval){
+  var isNegative = false;
+  if (eval <0){
+    isNegative = true;
+    eval = eval * -1;
+  }
+
+  var divide = Math.floor(eval / base);
+  var remainder = eval % base;
+  if (base > 10 && remainder >10){
+    remainder = letterConvert(remainder);
+  }
+  var resultString = remainder.toString();
+
+  while(divide >1){
+    var newDivide = Math.floor(divide / base);
+    remainder = divide % base;
+
+    if (base > 10 && remainder >10){
+      remainder = letterConvert(remainder);
+    }
+    resultString = remainder.toString() + resultString.toString();
+    divide = newDivide;
+  }
+  if (divide === 1){
+    resultString = divide.toString() + resultString.toString();
+    return resultString;
+  }
+  if(isNegative){
+    resultString = "-" + resultString;
+  }
+  return resultString;
+}
+
+function letterConvert(number){
+  var letterArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var index = number - 10;
+  return letterArray[index];
+}
+
+$(document).ready(function(){
+  $("#form1").submit(function(event){
+    event.preventDefault();
+
+    var baseCountNumber = $("#baseNumber").val();
+    var evaluateNumber = $("#evalNumber").val();
+
+    if (baseCountNumber > 62){
+      alert("Base is too big!");
+    }
+
+    var result = convert(baseCountNumber, evaluateNumber);
+    console.log(result);
+
+    $(".output").show();
+    $("#newNum").text(result);
+  })
+})
